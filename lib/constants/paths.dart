0d85a04ec57e3b61e4e07e2aa4d3188b18f471a8
Gregory <blackarcanjus@gmail.com>
2024-03-24 13:53:23 -0400

    [FIX] wrong root path

--- lib/constants/paths.dart
+++ lib/constants/paths.dart
@@ -1,7 +1,6 @@
-const WINE_PATH = "D:/batoenv/userdata/system/wine";
-//const WINE_PATH = "/userdata/system/wine";
+//const WINE_PATH = "D:/batoenv/userdata/system/wine";
+const WINE_PATH = "/userdata/system/wine";
 const REDIST_PATH = "$WINE_PATH/exe";
 const REDIST_PATH_DISABLED = "$WINE_PATH/exe.bak";
 const PROTON_OVERRIDE_PATH = "$WINE_PATH/ge-custom";
 const PROTONS_PATH = "$WINE_PATH/protons";
-
