398ec1f79e628342ebb633f64851c5a1cdb071ef
Gregory Cabral De Los Santos <Blackarcanjus@gmail.com>
2024-06-03 17:53:36 -0400

    Update update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,6 +1,6 @@
 WINE_MANAGER_EXEC="/userdata/system/wine_manager/batocera_wine_manager"
 echo "updating..."
-curl -L links.gregoryc.dev/wine-manager | bash
+curl -L cdn.gregoryc.dev/wine-manager | bash
 echo "running..."
 if [ -f "$WINE_MANAGER_EXEC" ]; then
   "$WINE_MANAGER_EXEC"
